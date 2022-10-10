e4971153baf945dd4e1089dfce1b1be1c64640f2
Sabrina Escalona <sabrina.escalona@Sabrinas-MacBook-Pro.local>
2022-10-09 18:26:47 -0700

    Refactored Search bar to use mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from "react";
 import Background from './Background.js';
-
+import SearchBar from "material-ui-search-bar";
 
 function App() {
   const API_KEY = "1454ed3aaede7b9e8409f598962c9b74";
@@ -9,33 +9,41 @@ function App() {
   const [data, setData] = useState([]);
   const[city, setCity] = useState([]);
 
-  const search = event => {
-    if(event.key === 'Enter') {
-      fetch(`${API_URL}/weather?q=${event.target.value}&appid=${API_KEY}&units=imperial`)
+  const search = city => {
+      fetch(`${API_URL}/weather?q=${city}&appid=${API_KEY}&units=imperial`)
       .then(res => res.json())
       .then(result => {
         switch(result.cod) {
           case "404":
-            alert("ERROR: City does not exist");
+            alert("City does not exist, please try again.");
+            break;
           default:
             setData(result);
             setCity("");
         }
         console.warn("Fetched", result);
-      });
-    }
+    });
   }
 
   return (
     <div className="weatherApp">
-        <input 
-          type="text" 
-          className="search-bar" 
+      <div className="searchBar">
+        <SearchBar 
           placeholder='Enter your location'
-          onChange={e => setCity(e.target.value)}
           value={city} 
-          onKeyPress={search} 
-        />
+          onRequestSearch={search}
+          style={{
+            'padding': '15px',
+            'margin': '10px 10px 10px 10px',
+            'width': '97%',
+            'backgroundColor': 'rgba(255,255,255,0.5)',
+            'boxShadow': '3px 3px 3px 3px rgba(39, 35, 35, 0.3)',
+            'borderRadius': '40px',
+            'fontSize': 'large',
+
+          }}
+        ></SearchBar>
+        </div>
         {data.name ? (
           <Background weatherData={data}></Background>
         ) : console.log("Waiting for data...")}
